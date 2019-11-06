d2015607982a3faf13ccc209620c21268127350e
Fadi Serapian <fadi.serapian@gmail.com>
2019-11-06 11:28:44 -0500

    Name Updated

--- app/config/config.php
+++ app/config/config.php
@@ -3,13 +3,13 @@
 define('DB_USER', 'root');
 define('DB_PASS', '120130140');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'postshare');
+define('DB_NAME', 'proshare');
 
 // app root
 define('APPROOT', dirname(dirname(__FILE__)));
 
 // URL root
-define('URLROOT', 'http://localhost:8080/postshare');
+define('URLROOT', 'http://localhost:8080/proshare');
 
 // site name
 define('SITENAME', 'ProShare');
